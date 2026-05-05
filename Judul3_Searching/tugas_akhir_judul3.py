be9fc6d93ac445f9773345bb7b67f891efca7940
May Devi Amalia <maydeviamaliaa66@gmail.com>
2026-05-05 10:15:10 +0700

    Add files via upload

---
+++ Judul3_Searching/tugas_akhir_judul3.py
@@ -0,0 +1,43 @@
+def binary_search(arr, n, target):
+    l = 0
+    r = n - 1
+    pos = -1
+    while l <= r:
+        m = l + (r - l) // 2
+        print(f"Median: {m}, nilai: {arr[m]}")
+        if arr[m] == target:
+            pos = m
+            break
+        elif arr[m] < target:
+            print("Mencari di kanan")
+            l = m + 1
+        else:
+            print("Mencari di kiri")
+            r = m - 1
+    return pos
+
+
+def main():
+    data = [2, 5, 8, 12, 15, 18, 22, 25]
+    n = len(data)
+    baris_depan = "Baris depan"
+    baris_belakang = "Baris belakang"
+    print(f"Array: {data}")
+    while True:
+        try:
+            target = int(input("Masukkan nomor kursi yang ingin dicari: "))
+            break
+        except ValueError:
+            print("Input tidak valid, silakan masukkan angka!")
+    pos = binary_search(data, n, target)
+    if pos != -1:
+        if 0 <= pos <= 3:
+            print(f"Kursi {target} ditemukan pada indeks ke-{pos} maka, posisi baris depan")
+        if 4 <= pos <= 7:
+            print(f"Kursi {target} ditemukan pada indeks ke-{pos} maka, posisi baris belakang")
+    else:
+        print("Nomor kursi tidak ditemukan dalam daftar.")
+
+
+if __name__ == "__main__":
+    main()
